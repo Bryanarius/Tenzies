66a0c3c0e2e842ae4eceaa165998cfa9ce519aa8
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2022-08-15 04:31:36 -0400

    Completed V1
    - Connected lose ends
    
    - Created a functional new game button
    
    - Added confetti dependency to add a flair to winning

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Die from './components/Die'
 import {nanoid} from "nanoid"
+import Confetti from "react-confetti"
 
 export default function App() {
 
@@ -37,36 +38,53 @@ export default function App() {
   }
 
 
-
   function rollDice() {
+   if(!tenzies) { 
     setDice(oldDice => oldDice.map(die => {
       return die.isHeld ?
         die :
         generateNewDie()
     }))
-  } 
+  } else {
+      setTenzies(false)
+      setDice(allNewDice())
+  }
+}
 
   function holdDice(id) {
     setDice(oldDice => oldDice.map(die => {
-
-      return die.id === id ? {...die, isHeld: !die.isHeld} :
+      return die.id === id ? 
+      {...die, isHeld: !die.isHeld} :
       die
     }))
     console.log(id)
   }
 
   const diceElements = dice.map(die => (
-  <Die key={die.id} value={die.value}  isHeld={die.isHeld} holdDice={() => holdDice(die.id)}/>
+  <Die 
+  key={die.id} 
+  value={die.value} 
+  isHeld={die.isHeld} 
+  holdDice={() => holdDice(die.id)}
+    />
   ))
 
   return (
     <main>
+      {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
-            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+
       <div className="dice-container">
         {diceElements}
       </div>
-      <button className="roll-dice" onClick={rollDice}>Roll</button>
+      
+      <button 
+        className="roll-dice"
+        onClick={rollDice}
+      >
+          {tenzies ? "New Game" : "Roll"}
+      </button>
     </main>
   )
 }
